6efb05a620ee6535ae6e84ecf9edf3c677cddd01
Andrew Arrow <oneone@gmail.com>
2021-07-31 16:30:39 -0700

    a byte is a number from 0 to 255

--- 001/main.go
+++ 001/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// go365 lesson 001: a byte is a number from 0 to 255
+
 func main() {
 	fmt.Println("")
 	fmt.Println("a byte is a number from 0 to 255")
@@ -15,7 +17,7 @@ func main() {
 	fmt.Println(b1, b2)
 	fmt.Println("")
 	fmt.Println("This is easier to understand if we look at the binary zeros and ones")
-	fmt.Printf("%b %b\n", b1, b2)
+	fmt.Printf("%b %b\n", b1, b2) // with Printf vs Println and %b you can tell go to print the zeros and ones
 	fmt.Println("")
 	fmt.Println("A byte has 8 possible zeros or ones and 11111111 is the max")
 	fmt.Println("If you add 1 it goes back to 00000000 or 0")
@@ -34,5 +36,8 @@ func main() {
 	fmt.Println("128+1=129")
 	fmt.Println("")
 	fmt.Println("Notice how 129 can be made with 128 on and everything else off except the last 1")
+	fmt.Println("")
+	b3 := byte(129)
+	fmt.Println(b3) // change this to Printf and use %b to show 129 in binary
 	fmt.Println("")
 }
