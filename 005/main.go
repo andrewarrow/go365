8f5f0af7feb6588cbc7f92fab73972458cce1717
Andrew Arrow <oneone@gmail.com>
2021-08-07 18:33:56 -0700

    comments on 005

--- 005/main.go
+++ 005/main.go
@@ -67,3 +67,49 @@ func ReadInZerosAndOnes(ip string, conn net.Conn) {
 		// try it with just %b and see the difference
 	}
 }
+
+// as you run this and see output like
+
+/*
+
+making a connection
+connection established: [::1]:8080
+[::1]:62435
+Receieved [00001111] from client [::1]:62435
+Receieved [10110000] from client [::1]:62435
+Receieved [11110001] from client [::1]:62435
+making a connection
+connection established: [::1]:8080
+[::1]:62436
+Receieved [01101011] from client [::1]:62436
+Receieved [00100000] from client [::1]:62435
+Receieved [10100000] from client [::1]:62435
+Receieved [00101111] from client [::1]:62436
+Receieved [01000111] from client [::1]:62435
+Receieved [11111010] from client [::1]:62436
+making a connection
+Receieved [11101011] from client [::1]:62436
+Receieved [01110011] from client [::1]:62435
+[::1]:62439
+connection established: [::1]:8080
+Receieved [01100011] from client [::1]:62439
+
+*/
+
+// Let's interpret what this is saying:
+
+// we see "making a connection"
+// then "connection established" on port 8080
+// 62435 is the part of the ip address of the client connecting
+// then we get three messages from this client, 00001111,10110000,11110001
+// then "making a connection" and "connection established" again on port 8080
+// ah it's somebody new 62436
+// notice from this new client we get 01101011,00101111,11111010
+// but also notice I had to find these mixed in with more data from 62435
+// 00100000,10100000,01000111
+// They are sending these data simultaneously
+// Then we get 62439 and the output stops here
+
+// add more fmt.Println statements in this program in various
+// places and change the output, run it over and over until
+// you feel like 006
